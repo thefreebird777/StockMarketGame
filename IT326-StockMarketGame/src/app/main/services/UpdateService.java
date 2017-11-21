d9e8d5b595f1515b05067e7cc417e892deb3137b
Ian Leyden <thefreebird777@gmail.com>
2017-11-21 14:09:52 -0600

    Update UpdateService.java
    got rid of getData() because function  was moved to StockAPIService

--- IT326-StockMarketGame/src/app/main/services/UpdateService.java
+++ IT326-StockMarketGame/src/app/main/services/UpdateService.java
@@ -2,11 +2,6 @@ package app.main.services;
 
 import java.util.HashMap;
 
-import org.apache.http.*;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
-
 public class UpdateService {
 
 	private static HashMap<String, Double> marketTable;
@@ -26,21 +21,5 @@ public class UpdateService {
 
 	private void initMarketTable() {
 
-	}
-
-	public void getData(String symbol){
-		DefaultHttpClient httpclient = new DefaultHttpClient();
-		try {
-			HttpHost target = new HttpHost("www.alphavantage.co", 80, "https");
-			HttpGet getRequest = new HttpGet("/query?function=TIME_SERIES_INTRADAY&symbol="+ symbol + "&interval=15min&apikey=T43MK3VWRNJ7TNH8");
-
-			HttpResponse httpResponse = httpclient.execute(target, getRequest);
-			HttpEntity entity = httpResponse.getEntity();
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			httpclient.getConnectionManager().shutdown();
-		}
 	}
 }
