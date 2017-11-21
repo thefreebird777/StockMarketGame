e27df3e0b61e3f38efa5c8c2e293b6c49f3e634b
Ian Leyden <thefreebird777@gmail.com>
2017-11-21 15:24:55 -0600

    parsed getData HTTP Entity to JSON

--- IT326-StockMarketGame/src/app/main/services/StockAPIService.java
+++ IT326-StockMarketGame/src/app/main/services/StockAPIService.java
@@ -5,6 +5,8 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -15,7 +17,7 @@ public class StockAPIService {
     private String URL = "www.alphavantage.co";
     private String  KEY = "&interval=15min&apikey=T43MK3VWRNJ7TNH8";
     private String QUERY = "/query?function=TIME_SERIES_INTRADAY&symbol=";
-    private StockService STOCK_SERVICE = new StockService();
+//    private StockService STOCK_SERVICE = new StockService();
     private ArrayList<String> tickerList;
     private ScheduledExecutorService exec = Executors.newScheduledThreadPool(1);
 
@@ -23,22 +25,26 @@ public class StockAPIService {
         exec.scheduleAtFixedRate(runnable , 0, 15, TimeUnit.MINUTES);
     }
 
-    public HttpEntity getData(String symbol){
+    public JSONObject getData(String symbol){
         DefaultHttpClient httpclient = new DefaultHttpClient();
         HttpEntity entity = null;
+        String s = "";
+        JSONObject jsonObj = null;
         try {
             HttpHost target = new HttpHost(URL, 80, "http");
             HttpGet getRequest = new HttpGet(QUERY + symbol + KEY);
 
             HttpResponse httpResponse = httpclient.execute(target, getRequest);
-            entity = httpResponse.getEntity();
+            entity = httpResponse.getEntity(); //api returns HTTP Entity
+            s = EntityUtils.toString(entity); //parse entity to String
+            jsonObj = new JSONObject(s.toString()); //pasrse String to JSON
 
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
             httpclient.getConnectionManager().shutdown();
         }
-        return entity;
+        return jsonObj;
     }
 
     Runnable runnable = new Runnable() {
